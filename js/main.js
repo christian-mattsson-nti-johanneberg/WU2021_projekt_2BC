7bf1d18a97192a633637e54db491b44090ac431b
christian-mattsson-nti-johanneberg <christian.mattsson@elev.ga.ntig.se>
2021-03-01 20:18:19 +0100

    Updates to index.html

--- js/main.js
+++ js/main.js
@@ -37,6 +37,7 @@ for (let btn of buttons)  {
   });
 }
 
+
 /* Text Animations */
 anime.timeline({loop: false})
   .add({
@@ -46,4 +47,83 @@ anime.timeline({loop: false})
     easing: "easeOutCirc",
     duration: 800,
     delay: (el, i) => 800 * i
+  });
+
+
+  /* Cloud Upload Animation */
+anime.timeline({loop: true})
+  .add({
+    targets: '.laptop',
+    opacity: [0, 1],
+    translateX: ["50%", 0],
+    delay: 1000,
+    duration: 1000,
+  })
+  .add({
+    targets: '.cloud',
+    scale: [0, 1],
+    delay: 100,
+    duration: 1000,
+  })
+  .add({
+    targets: '.upload-circle',
+    scale: [0, 1],
+    delay: anime.stagger(150, {direction: 'reverse'}),
+    duration: 250,
+    easing: 'easeOutExpo',
+  })
+  .add({
+    targets: '.upload-circle',
+    scale: [1, 1.5, 1],
+    delay: anime.stagger(150, {direction: 'reverse'}, {start: 250}),
+    duration: 750,
+    easing: 'linear',
+  })
+  .add({
+    targets: '.upload-circle',
+    scale: [1, 1.5, 1],
+    delay: anime.stagger(150, {direction: 'reverse'}),
+    duration: 750,
+    easing: 'linear',
+  })
+  .add({
+    targets: '.upload-circle',
+    scale: [1, 1.5, 1],
+    delay: anime.stagger(150, {direction: 'reverse'}),
+    duration: 750,
+    easing: 'linear',
+  })
+  .add({
+    targets: '.check',
+    width: [0, '25%'],
+    duration: 750,
+    easing: 'linear',
+  })
+  .add({
+    targets: '.check',
+    width: ['25%', 0],
+    delay: 500,
+    duration: 1250,
+    easing: 'linear',
+  })
+  .add({
+    targets: ['.check', '.cloud'],
+    scale: [1, 0],
+    delay: 500,
+    duration: 500,
+    easing: 'easeOutExpo'
+  })
+  .add({
+    targets: '.upload-circle',
+    scale: [1, 0],
+    delay: anime.stagger(150),
+    duration: 250,
+    easing: 'easeOutExpo',
+  })
+  .add({
+    targets: '.laptop',
+    opacity: [1, 0],
+    translateX: [0, "-50%"],
+    duration: 1000,
+    easing: 'easeOutExpo',
   });
